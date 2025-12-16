84e10a988e51859ad5f78dc8159e6ee99b4aa511
Thomas aussenac <thomauss@gmail.com>
2025-12-16 16:47:43 +0100

    init files

---
+++ label.gd
@@ -0,0 +1,15 @@
+class_name MonLabel
+extends Label
+
+@export var toto: int=1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("moi %s" % name)
+	var monlabel2: MonLabel= $"../Label2"
+	monlabel2.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
